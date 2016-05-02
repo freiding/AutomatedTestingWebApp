16d079766f1a9608e9dc120cf4a745e8b3e3af32
Bogatyrev Evgenij <bogatyrev.evgenij@gmail.com>
2016-05-02 20:56:11 +0300

    This class stores information for tests

--- src/test/java/core/TestData.java
+++ src/test/java/core/TestData.java
@@ -4,4 +4,17 @@ package core;
  * Created by bogat on 5/2/2016.
  */
 public class TestData {
+
+    /*
+    * User data
+    */
+    public final static String EMAIL = "user5684931@gmail.com";
+    public final static String PASSWORD = "password477100321";
+    public final static String USERNAME = "Assistant";
+
+    /*
+    * Post data
+    */
+    public final static String POST_TEXT = "This is text for my test post";
+
 }
