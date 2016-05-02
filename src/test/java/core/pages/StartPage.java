bef5733cb768453dd5c328052580869a7e0dac2c
Bogatyrev Evgenij <bogatyrev.evgenij@gmail.com>
2016-05-02 21:02:30 +0300

    # Added next WebElemnts: ## buttonAddPost ## lastPostText ## lastPostAuthor ## postMenuButton - not used because is hidden ## postMenuItemDelete - not used because is hidden
    # Refactoring code

--- src/test/java/core/pages/StartPage.java
+++ src/test/java/core/pages/StartPage.java
@@ -17,8 +17,18 @@ public class StartPage extends AbstractPage {
     private WebElement postField;
     @FindBy(xpath = ".//*[@id='contentPane']/div/div[3]/div/div[2]/div[1]/div/div[2]/div[1]/div[7]/div[2]/div[2]/div/div[2]/div[2]")
     private WebElement messageField;
-    //
+    @FindBy(xpath = ".//*[@id='contentPane']/div/div[3]/div/div[2]/div[1]/div/div[2]/div[2]/table/tbody/tr/td[1]/div[1]")
+    private WebElement buttonAddPost;
+    @FindBy(xpath = ".//*[@id='contentPane']/div/div[3]/div/div[2]/div[2]/div/div[1]/div[2]/div[2]/div/div[2]/div/div/div/div[2]")
+    private WebElement lastPostText;
+    @FindBy(xpath = ".//*[@id='contentPane']/div/div[3]/div/div[2]/div[2]/div/div[1]/div[2]/div[2]/div/div[1]/div/header/h3/a")
+    private WebElement lastPostAuthor;
+    @FindBy(xpath = ".//*[@id='contentPane']/div/div[3]/div/div[2]/div[2]/div/div[1]/div[2]/div[2]/div[1]/div[1]/span")
+    private WebElement postMenuButton;
+    @FindBy(xpath = ".//*[@id='contentPane']/div/div[3]/div/div[2]/div[2]/div/div[1]/div[2]/div[3]/div[2]/div")
+    private WebElement postMenuItemDelete;
 
+    //  .//*[@id=':5v']/div
     public StartPage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(this.driver, this);
@@ -29,15 +39,47 @@ public class StartPage extends AbstractPage {
         driver.navigate().to(PAGE_URL);
     }
 
-    public void setMessageFieldText(String message) {
-        messageField.sendKeys(message);
-    }
+    /************************************************
+     * Pressers
+     ************************************************/
 
     public void pressPostField() {
         postField.click();
     }
 
+    public void pressButtonAddPost() {
+        buttonAddPost.click();
+    }
+
+    public void pressPostMenuButton() {
+        postMenuButton.click();
+    }
+
+    public void pressPostMenuItemDelete() {
+        postMenuItemDelete.click();
+    }
+
+    /************************************************
+     * Getters
+     ************************************************/
     public String getUsername() {
         return usernameLabel.getText();
+    }
+
+    public String getLastPostAuthor() {
+        return lastPostAuthor.getText();
+    }
+
+    public String getLastPostText() {
+        return lastPostText.getText();
+    }
+
+    /************************************************
+     * Setters
+     ************************************************/
+
+    public void setMessageFieldText(String message) {
+        messageField.sendKeys(message);
+        PageFactory.initElements(this.driver, this);
     }
 }
