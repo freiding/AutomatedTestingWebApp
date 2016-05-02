c3a1c307b75b9d50921f3f46e9af160da114f9ad
Bogatyrev Evgenij <bogatyrev.evgenij@gmail.com>
2016-05-02 20:56:48 +0300

    add class Post to git

--- src/test/java/core/objects/Post.java
+++ src/test/java/core/objects/Post.java
@@ -4,4 +4,20 @@ package core.objects;
  * Created by bogat on 5/2/2016.
  */
 public class Post {
+
+    private String text;
+    private User author;
+
+    public Post(User author, String text) {
+        this.author = author;
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
 }
